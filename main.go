00de52ef946053eec9079744ad413cd199f3bc0b
Chamod Shehanka <hcsperera@outlook.com>
2020-07-01 22:02:59 +0530

    mongo

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
 }
